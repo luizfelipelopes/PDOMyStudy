5fe8832019e8ea5a6124b88d7e3c0c88edb8621d
Luiz Felipe <lfelipelopesti@gmail.com>
2019-11-14 12:30:57 -0300

    Put settings connection BD in the file

---
+++ Config.php
@@ -0,0 +1,10 @@
+<?php
+
+define('INFO_BD', [
+	'driver' => 'mysql',
+	'dbname' => 'test',
+	'host' => 'localhost',
+	'user' => 'root',
+	'pass' => '',
+	'attributes' => [PDO::ATTR_PERSISTENT => true],
+]);
