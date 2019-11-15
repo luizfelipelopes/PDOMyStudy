b9bbb55f47f5cf7712fb9d503a559a886e74b8fd
Luiz Felipe <lfelipelopesti@gmail.com>
2019-11-15 10:45:38 -0300

    working with beginTransaction, exec, rollBack and commit methods

---
+++ PDOMethods.php
@@ -0,0 +1,49 @@
+<?php
+
+require 'Config.php';
+
+echo '<h1>Methods PDO</h1>';
+
+
+/**
+* Connection BD
+**/
+
+try {
+
+	$db = new PDO(INFO_BD['driver'] . ':host=' . INFO_BD['host'] . ';dbname=' . INFO_BD['dbname'], 
+		INFO_BD['user'], INFO_BD['pass'], INFO_BD['attributes']);
+
+	echo 'Conected!<br>';
+	
+} catch (PDOException $e) {
+	
+	die("Error!:" . $e->getMessage());
+}
+
+
+/**
+* PDO::beginTransaction()
+* PDO::exec()
+* PDO::rollBack()
+* PDO::commit()
+**/
+
+$db->beginTransaction();
+
+$sth = $db->exec('CREATE TABLE lunch 
+(id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR(255) NOT NULL,
+datecreate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+dateupdate TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP)'
+);
+
+$sth = $db->exec('DROP TABLE lunch');
+$sth = $db->exec('INSERT INTO dessert (name) VALUES ("tomato")');
+$sth = $db->exec('UPDATE dessert SET name = "hamburguer"');
+
+$db->rollBack();
+// $db->commit();
+
+
+
